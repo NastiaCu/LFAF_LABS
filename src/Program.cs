54094f592f81bea6096d896213d3318a9bae6957
NastiaCu <nastiacu24@gmail.com>
2023-03-27 20:05:24 +0300

    lab 3 commit

--- src/Program.cs
+++ src/Program.cs
@@ -1,93 +1,123 @@
 ﻿using System;
 using static System.Text.Json.JsonSerializer;
 using System.Linq;
-
+using System.Collections.Generic;
 
 namespace LFAF_LABS{
 
     class Program{
         public static void Main(String[] args){
-            //LAB_1
-            Grammar grammar = new Grammar();
-            grammar.AddVN("S");
-            grammar.AddVN("B");
-            grammar.AddVN("D");
-            grammar.AddVT("a");
-            grammar.AddVT("b");
-            grammar.AddVT("c");
-            grammar.AddProduction("S", "aB");
-            grammar.AddProduction("S", "bB");
-            grammar.AddProduction("B", "bD");
-            grammar.AddProduction("B", "cB");
-            grammar.AddProduction("B", "aS");
-            grammar.AddProduction("D", "aD");
-            grammar.AddProduction("D", "b");
-            grammar.AddStart("S");
+            // //LAB_1
+            // Grammar grammar = new Grammar();
+            // grammar.AddVN("S");
+            // grammar.AddVN("B");
+            // grammar.AddVN("D");
+            // grammar.AddVT("a");
+            // grammar.AddVT("b");
+            // grammar.AddVT("c");
+            // grammar.AddProduction("S", "aB");
+            // grammar.AddProduction("S", "bB");
+            // grammar.AddProduction("B", "bD");
+            // grammar.AddProduction("B", "cB");
+            // grammar.AddProduction("B", "aS");
+            // grammar.AddProduction("D", "aD");
+            // grammar.AddProduction("D", "b");
+            // grammar.AddStart("S");
 
-            grammar.GenerateString();
-            grammar.GenerateString();
-            grammar.GenerateString();
-            grammar.GenerateString();
-            grammar.GenerateString();            
-            FiniteAutomaton Automaton = grammar.ToFiniteAutomaton();
+            // grammar.GenerateString();
+            // grammar.GenerateString();
+            // grammar.GenerateString();
+            // grammar.GenerateString();
+            // grammar.GenerateString();            
+            // FiniteAutomaton Automaton = grammar.ToFiniteAutomaton();
             
-            string s = "acbab";
+            // string s = "acbab";
 
-            if (Automaton.isValid(s)){
-                Console.WriteLine(s + " is a valid string");
+            // if (Automaton.isValid(s)){
+            //     Console.WriteLine(s + " is a valid string");
+            // }
+            // else Console.WriteLine(s + "is an invalid string");
+            // Console.WriteLine();
+
+            // //LAB_2
+            // grammar.typeDefinition();
+            // FiniteAutomaton automaton = new FiniteAutomaton();
+            // automaton.AddState("q0");
+            // automaton.AddState("q1");
+            // automaton.AddState("q2");
+            // automaton.AddState("q3");
+            // automaton.AddAlphabet("a");
+            // automaton.AddAlphabet("b");
+            // automaton.AddAlphabet("c");
+            // automaton.AddTransition("q0", "aq1");
+            // automaton.AddTransition("q1", "bq2");
+            // automaton.AddTransition("q2", "cq0");
+            // automaton.AddTransition("q1", "aq3");
+            // automaton.AddTransition("q0", "bq2");
+            // automaton.AddTransition("q2", "cq3");
+            // automaton.AddInitialState("q0");
+            // automaton.AddFinalState("q3");
+
+            // Console.WriteLine("Convertion of a FA to a Regular Grammar");
+            // Grammar RG = automaton.ToGrammar();
+            // Console.WriteLine("Set of non-terminal symbols: " + Serialize(RG.VN));
+            // Console.WriteLine("Set of terminal symbols: " + Serialize(RG.VT));
+            // Console.WriteLine("Set of productions: " + Serialize(RG.P));
+            // Console.WriteLine();
+            // automaton.isDeterministic();
+
+            // FiniteAutomaton a = new FiniteAutomaton();
+            // a.AddState("q0");
+            // a.AddState("q1");
+            // a.AddState("q2");
+            // a.AddState("q3");
+            // a.AddAlphabet("a");
+            // a.AddAlphabet("b");
+            // a.AddAlphabet("c");
+            // a.AddTransition2("q0", "a", "q1");
+            // a.AddTransition2("q1", "b", "q2");
+            // a.AddTransition2("q2", "c", "q0");
+            // a.AddTransition2("q1", "a", "q3");
+            // a.AddTransition2("q0", "b", "q2");
+            // a.AddTransition2("q2", "c", "q3");
+            // a.AddInitialState("q0");
+            // a.AddFinalState("q3");
+            // Console.WriteLine("The NFA has the following set of transitions: " + Serialize(a.transitions2 ) );
+            // Console.WriteLine();
+
+            // Console.WriteLine("Convertion of the NFA to DFA");
+            // a.ConvertToDFA(a);
+            // a.ToGraph("fa.dot");       
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+           
+            // //LAB_3
+            string program = @"
+                function func(){
+                    var x = 34;
+                    var y = 55;
+                    var string = ""hello world"";
+
+                    if (x != y){
+                        return y;
+                    }
+
+                    else{
+                        for (int i = 0; i < 10; i++) {
+                        x++;
+                    }
+                    break;
+                }
+
+            ";
+
+            Lexer l = new Lexer();
+
+            List<(string, string)> tokens = l.Tokenize(program);
+
+            foreach ((string tokenName, string tokenValue) in tokens){
+                Console.WriteLine($"Token: ({tokenName} : {tokenValue})");
             }
-            else Console.WriteLine(s + "is an invalid string");
-            Console.WriteLine();
-
-            //LAB_2
-            grammar.typeDefinition();
-            FiniteAutomaton automaton = new FiniteAutomaton();
-            automaton.AddState("q0");
-            automaton.AddState("q1");
-            automaton.AddState("q2");
-            automaton.AddState("q3");
-            automaton.AddAlphabet("a");
-            automaton.AddAlphabet("b");
-            automaton.AddAlphabet("c");
-            automaton.AddTransition("q0", "aq1");
-            automaton.AddTransition("q1", "bq2");
-            automaton.AddTransition("q2", "cq0");
-            automaton.AddTransition("q1", "aq3");
-            automaton.AddTransition("q0", "bq2");
-            automaton.AddTransition("q2", "cq3");
-            automaton.AddInitialState("q0");
-            automaton.AddFinalState("q3");
-
-            Console.WriteLine("Convertion of a FA to a Regular Grammar");
-            Grammar RG = automaton.ToGrammar();
-            Console.WriteLine("Set of non-terminal symbols: " + Serialize(RG.VN));
-            Console.WriteLine("Set of terminal symbols: " + Serialize(RG.VT));
-            Console.WriteLine("Set of productions: " + Serialize(RG.P));
-            Console.WriteLine();
-            automaton.isDeterministic();
-
-            FiniteAutomaton a = new FiniteAutomaton();
-            a.AddState("q0");
-            a.AddState("q1");
-            a.AddState("q2");
-            a.AddState("q3");
-            a.AddAlphabet("a");
-            a.AddAlphabet("b");
-            a.AddAlphabet("c");
-            a.AddTransition2("q0", "a", "q1");
-            a.AddTransition2("q1", "b", "q2");
-            a.AddTransition2("q2", "c", "q0");
-            a.AddTransition2("q1", "a", "q3");
-            a.AddTransition2("q0", "b", "q2");
-            a.AddTransition2("q2", "c", "q3");
-            a.AddInitialState("q0");
-            a.AddFinalState("q3");
-            Console.WriteLine("The NFA has the following set of transitions: " + Serialize(a.transitions2 ) );
-            Console.WriteLine();
-
-            Console.WriteLine("Convertion of the NFA to DFA");
-            a.ConvertToDFA(a);
-            a.ToGraph("fa.dot");          
         }
     }
 }
---
+++ src/lexer/lexer.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace LFAF_LABS{
+    class Lexer{
+        private static Dictionary<string, string> tokenPatterns = new Dictionary<string, string>(){
+            { "LPAREN", @"\(" },
+            { "RPAREN", @"\)" },
+            { "LBRACE", @"\{" },
+            { "RBRACE", @"\}" },
+            { "COMMA", @"\," },
+            { "ASSIGN", @"\=" },
+            { "SEMICOLON", @"\;" },
+            { "PLUS", @"\+" },
+            { "MINUS", @"\-" },
+            { "MULTIPLY", @"\*" },
+            { "DIVIDE", @"\/" },
+            { "GREATER", @"\>" },
+            { "GREATER_EQUAL", @"\>=" },
+            { "EQUAL", @"\==" },
+            { "LESS", @"\<" },
+            { "LESS_EQUAL", @"\<=" },
+            { "NOT_EQUAL", @"\!=" },
+            { "IF", @"if" },
+            { "ELSE", @"else" },
+            { "FOR", @"for" },
+            { "RETURN", @"return" },
+            { "BREAK", @"break" },
+            { "CONTINUE", @"continue" },
+            { "VAR", @"var" },
+            { "FUNCTION", @"function" },
+            { "ID", @"[a-zA-Z_]\w*" },
+            { "NUMBER", @"\d+(\.\d+)?" },
+            { "STRING", @"""[^""]*""" },
+            { "WHITESPACE", @"\s" },
+        };
+
+        private List<(string, Regex)> tokenRegexes = new List<(string, Regex)>();
+
+        public Lexer(){
+            foreach (KeyValuePair<string, string> pattern in tokenPatterns){
+                tokenRegexes.Add((pattern.Key, new Regex("^" + pattern.Value)));
+            }
+        }
+
+        public List<(string, string)> Tokenize(string program){
+            List<(string, string)> tokens = new List<(string, string)>();
+
+            while (!string.IsNullOrEmpty(program)){
+                bool match = false;
+                foreach ((string tokenName, Regex regex) in tokenRegexes){
+                    Match tokenMatch = regex.Match(program);
+                    if (tokenMatch.Success){
+                        (string, string) token = (tokenName, tokenMatch.Value);
+                        if (tokenName != "WHITESPACE"){
+                            tokens.Add(token);
+                        }
+                        program = program.Substring(tokenMatch.Length);
+                        match = true;
+                        break;
+                    }
+                }
+                if (!match){
+                    Console.WriteLine("Invalid syntax: " + program);
+                }
+            }
+            return tokens;
+        }
+    }
+}
